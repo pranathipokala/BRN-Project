a2dbd9b6b67bf097d00425373f0e20ec06e6da6e
Pokala Pranathi <pranathipokala360@achiever-102.local>
2019-08-31 13:01:36 +0530

    Initial Commit

---
+++ BRN Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BRN Project
+//
+//  Created by Pokala Pranathi on 8/31/19.
+//  Copyright © 2019 Pokala Pranathi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
