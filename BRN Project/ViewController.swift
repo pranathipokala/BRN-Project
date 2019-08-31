a696cfec228095dc19e82d091add42d7f9b03e00
Pokala Pranathi <pranathipokala360@achiever-102.local>
2019-08-31 13:26:24 +0530

    last changes

--- BRN Project/ViewController.swift
+++ BRN Project/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("pranathi")
+        var d = 121
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
